56b002d9224a27cd0c0363843fc99ea8286d17cf
minhoo03 <minhoo0333@gmail.com>
2021-03-04 17:56:54 +0900

    react setting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import DrivePage from './components/DrivePage/DrivePage'
+import RegisterPage from './components/RegisterPage/RegisterPage'
+import LoginPage from './components/LoginPage/LoginPage'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Switch>
+          <Route exact path="/" component={DrivePage} />
+          <Route path="/register" component={RegisterPage} />
+          <Route path="/login" component={LoginPage} /> 
+        </Switch>
+    </Router>
   );
 }
 
