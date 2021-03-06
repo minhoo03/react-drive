b98afd81b712a5e60598b37cf542f0a1f0ebce8f
minhoo03 <minhoo0333@gmail.com>
2021-03-06 21:22:52 +0900

    Setting && Register Page

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,9 +1,62 @@
 import React from 'react'
+import { useRef } from 'react'
+import { useForm } from 'react-hook-form'
 
 export default function RegisterPage() {
+
+    const { register, watch, errors } = useForm({mode: 'onChange'})
+
+    const password = useRef()
+    password.current = watch('password')
+    console.log(watch('email','name','password'))
+
     return (
-        <div>
-            RegisterPage
+        <div className="auth_wrapper">
+            <div>
+                <h3>
+                    Register
+                </h3>
+            </div>
+
+            <form 
+            // onSubmit={handleSubmit(onSubmit)}
+            >
+                <label>Email</label>
+                <input
+                    name="email"
+                    type="email"
+                    ref={register({ required: true, pattern: /^\S+@\S+$/i})} 
+                />
+                {errors.email && <p>This field is required.</p>}
+
+                <label>Name</label>
+                <input
+                    name="name"
+                    type="text"
+                    ref={register({ required: true, maxLength: 10 })}
+                />
+                {errors.name && errors.name.type === 'required' && <p>This field is required.</p>}
+                {errors.name && errors.name.type === 'maxLength' && <p>Your inpuit exceed maximum length.</p>}
+
+                <label>Password</label>
+                <input
+                    name="password"
+                    type="password"
+                    ref={register({ required: true, minLength: 6 })}
+                />
+                {errors.password && errors.password.type === 'required' && <p>This field is required.</p>}
+                {errors.password && errors.password.type === 'minLength' && <p>Your input exceed minimum length.</p>}
+
+                <label>Password Confirm</label>
+                <input
+                    name="password_confirm"
+                    type="password"
+                    ref={register({ required: true, validate: value => value === password.current })}
+                />
+                {errors.password_confirm && <p>The password is not correct.</p>}
+
+                <input type="submit" />
+            </form>
         </div>
     )
 }
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+
+import 'firebase/auth'
+import 'firebase/storage'
+import 'firebase/database'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyB_sVR6AcHBW3i-YRXArFLUIWDwj08RDS0",
+    authDomain: "react-drive-5551a.firebaseapp.com",
+    projectId: "react-drive-5551a",
+    storageBucket: "react-drive-5551a.appspot.com",
+    messagingSenderId: "148276369622",
+    appId: "1:148276369622:web:3e7c0af9102d8cdd6cd177",
+    measurementId: "G-3XZ9CDV098"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
